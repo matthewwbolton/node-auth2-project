68ad37c9264a7c6f93bae70b82c0155c17fd0c09
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-22 11:45:50 -0400

    commit changes

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,8 @@ const knex = require("knex");
 
 const knexfile = require("../knexfile");
 
-module.exports = knex(knexfile.development);
+const environment = process.env.NODE_ENV || "development";
+
+const db = knex(knexfile[environment]);
+
+module.exports = db;
